6403c5e53f7a89237ba41ad4f78e4b62131b953b
Omen <Omen@DESKTOP-0UAHONR>
2022-11-01 00:43:32 +0500

    Add project files.

---
+++ My Project RETROFIX/Pages/Critiques.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace My_Project_RETROFIX.Pages
+{
+    public class CritiquesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
